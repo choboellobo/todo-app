3090c0949eb51b47b9a9858f1063b229225538e1
Eduardo <choboellobo@gmail.com>
2021-08-12 09:09:11 +0200

    Login wip

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,10 +7,12 @@ export default {
     methods: {
         login() {
             // Se va a logear con un email y contraseña
+            // Validar un email correcto y una contraseña de mas de 6 caracteres;
+
+
+            // Guardar en la memoria del navegador el access_token y el refresh_token
+            // Si hay algun error mostrarlo en pantalla.
         }
-    },
-    mounted() {
-        
     }
 }
 </script>
